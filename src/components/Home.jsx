58641d375f6cc8ef085b79128d1e63329571d415
Kemdjul <kemalasanov25@gmail.com>
2022-12-15 19:27:19 +0100

    Scrolling breakthrough
    - finally discovered how to add in scroll animations...

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,26 @@
-import { getSpaceUntilMaxLength } from '@testing-library/user-event/dist/utils'
-import React from 'react'
-import { useEffect } from 'react'
-import Profile from '../assets/home-profile.jpg'
+import React from 'react';
+import { useInView } from 'react-intersection-observer';
+import Profile from '../assets/home-profile.jpg';
+import './dynamic.css';
 
 const Home = () => {
+  const { ref: textRef, inView: textIsVisible } = useInView();
+  
+
   return (
     <div className="h-screen w-full bg-background font-orbitron text-white justify-center content-center flex flex-col">
         {/* Container */}
-        <div className="grid grid-cols-2 justify-center items-center max-w-[1200px] mx-auto gap-12">
-          {/* Welcome text */}
-          <div className="text-right font-bold">
-            <h1 className="drop-shadow-neonPink">Ja sam Kem<span className="animate-flickerSlow text-[3.157rem] delay-50">a</span>l.</h1>
-            <h1 className="drop-shadow-neonBlue">Front<span className="animate-flickerSlow text-[3.157rem]">e</span>nd  <span className="animate-flickerFast text-[3.157rem]">d</span>evel<span className="animate-flickerFast delay-75 text-[3.157rem]">o</span>per.</h1>
-          </div>
-          {/* Picture */}
-          <div>
-            <img className="w-80 h-80 rounded-full object-cover" src={Profile} />
+        <div className={`${textIsVisible ? 'home' : ''}`} ref={textRef}>
+          <div className="grid grid-cols-2 justify-center items-center max-w-[1200px] mx-auto gap-12">
+            {/* Welcome text */}
+            <div>
+              <h1 className="drop-shadow-neonPink text-right font-bold">Ja sam Kem<span className="animate-flickerSlow text-[3.157rem] delay-50">a</span>l.</h1>
+              <h1 className="drop-shadow-neonBlue text-right font-bold">Front<span className="animate-flickerSlow text-[3.157rem]">e</span>nd  <span className="animate-flickerFast text-[3.157rem]">d</span>evel<span className="animate-flickerFast delay-75 text-[3.157rem]">o</span>per.</h1>
+            </div>
+            {/* Picture */}
+            <div>
+              <img className="w-80 h-80 rounded-full object-cover" src={Profile} />
+            </div>
           </div>
         </div>
     </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,124 +8,122 @@ import FigmaLogo from '../assets/figma-logo.png'
 import IllustratorLogo from '../assets/illustrator-logo.png'
 import GitLogo from '../assets/git-logo.png'
 import GimpLogo from '../assets/gimp-logo.png'
+import { useInView } from 'react-intersection-observer'
 
 const Skills = () => {
-  /*
-  const Development = [
-    {id: "1", name: "HTML", percent: "75%", bar: "w-[18rem]"},
-    {id: "2", name: "CSS", percent: "80%", bar: "w-[19.2rem]"},
-    {id: "3", name: "React", percent: "70%", bar: "w-[16.8rem]"},
-    {id: "4", name: "Javascript", percent: "80%", bar: "w-[19.2rem]"},
-    {id: "5", name: "C++", percent: "90%", bar: "w-[21.6rem]"},
-  ];
-*/
+  const { ref: skillsRef, inView: skillsVisible } = useInView();
+
   return (
     <div className="bg-background text-white h-screen w-full flex justify-center items-center">
         {/* Container */}
         <div className="max-w-[90%] h-[90%] text-center">
             <h2 className="mb-8 font-bold font-orbitron drop-shadow-neonBlue">Skills</h2>
+            <div className = { `${skillsVisible ? 'skills' : '' }`}>
             {/* Grid container */}
-            <div class="grid grid-cols-2 gap-20 ">
-                {/* Development */}
+              <div class="grid grid-cols-2 gap-20 " ref = {skillsRef}>
+                  
+                  
+                  {/* Development */}
 
-                <section className="w-[100%] flex flex-col">
+                  <section className="w-[100%] flex flex-col">
+
+                    <div className="flex items-center mt-4">
+                      <img className="w-14 mr-2" src={HtmlLogo} alt="Html 5"/>
+                      <div className="flex w-[90%] h-[100%] items-center">
+                        <div className="w-96 h-1 bg-gray-600"></div>
+                        <div className="w-[19.2rem] h-1 bg-primary absolute drop-shadow-neonPink"></div>
+                        <div className="h-1 w-[19.2rem] rounded-full bg-gray-200 absolute"></div>
+                      </div>
+                      <p className="ml-4 rounded-full">80%</p>
+                    </div>
+
+                    <div className="flex items-center mt-4">
+                      <img className="w-14 mr-2" src={CSSLogo} alt="CSS"/>
+                      <div className="flex w-[90%] h-[100%] items-center">
+                        <div className="w-96 h-1 bg-gray-600"></div>
+                        <div className="w-[20.4rem] h-1 bg-primary absolute drop-shadow-neonPink"></div>
+                        <div className="h-1 w-[20.4rem] rounded-full bg-gray-200 absolute"></div>
+                      </div>
+                      <p className="ml-4 rounded-full">85%</p>
+                    </div>
+
+                    <div className="flex items-center mt-4">
+                      <img className="w-14 mr-2" src={JavascriptLogo} alt="Javascript"/>
+                      <div className="flex w-[90%] h-[100%] items-center">
+                        <div className="w-96 h-1 bg-gray-600"></div>
+                        <div className="w-[16.8rem] h-1 bg-primary absolute drop-shadow-neonPink"></div>
+                        <div className="h-1 w-[16.8rem] rounded-full bg-gray-200 absolute"></div>
+                      </div>
+                      <p className="ml-4 rounded-full">70%</p>
+                    </div>
+
+                    <div className="flex items-center mt-4">
+                      <img className="w-14 mr-2" src={ReactLogo} alt="React framework for Javascript"/>
+                      <div className="flex w-[90%] h-[100%] items-center">
+                        <div className="w-96 h-1 bg-gray-600"></div>
+                        <div className="w-[16.8rem] h-1 bg-primary absolute drop-shadow-neonPink"></div>
+                        <div className="h-1 w-[16.8rem] rounded-full bg-gray-200 absolute"></div>
+                      </div>
+                      <p className="ml-4 rounded-full">70%</p>
+                    </div>
+
+                    <div className="flex items-center mt-4">
+                      <img className="w-14 mr-2" src={TailwindLogo} alt="Tailwind framework for CSS"/>
+                      <div className="flex w-[90%] h-[100%] items-center">
+                        <div className="w-96 h-1 bg-gray-600"></div>
+                        <div className="w-[20.4rem] h-1 bg-primary absolute drop-shadow-neonPink"></div>
+                        <div className="h-1 w-[20.4rem] rounded-full bg-gray-200 absolute"></div>
+                      </div>
+                      <p className="ml-4 rounded-full">85%</p>
+                    </div>
+
+                  </section>
+
+                  {/* Design/Git */}
+                  <section className="w-[100%] flex flex-col justify-center">
 
                   <div className="flex items-center mt-4">
-                    <img className="w-14 mr-2" src={HtmlLogo} alt="Html 5"/>
-                    <div className="flex w-[90%] h-[100%] items-center">
-                      <div className="w-96 h-1 bg-gray-600"></div>
-                      <div className="w-[19.2rem] h-1 bg-primary absolute drop-shadow-neonPink"></div>
-                      <div className="h-1 w-[19.2rem] rounded-full bg-gray-200 absolute"></div>
+                      <img className="w-14 mr-2" src={GitLogo} alt="Git"/>
+                      <div className="flex w-[90%] h-[100%] items-center">
+                        <div className="w-96 h-1 bg-gray-600"></div>
+                        <div className="w-[18rem] h-1 bg-secondary absolute drop-shadow-neonBlue"></div>
+                        <div className="h-1 w-[18rem] rounded-full bg-gray-200 absolute"></div>
+                      </div>
+                      <p className="ml-4 rounded-full">75%</p>
                     </div>
-                    <p className="ml-4 rounded-full">80%</p>
-                  </div>
 
-                  <div className="flex items-center mt-4">
-                    <img className="w-14 mr-2" src={CSSLogo} alt="CSS"/>
-                    <div className="flex w-[90%] h-[100%] items-center">
-                      <div className="w-96 h-1 bg-gray-600"></div>
-                      <div className="w-[20.4rem] h-1 bg-primary absolute drop-shadow-neonPink"></div>
-                      <div className="h-1 w-[20.4rem] rounded-full bg-gray-200 absolute"></div>
+                    <div className="flex items-center mt-4">
+                      <img className="w-14 mr-2" src={FigmaLogo} alt="Figma"/>
+                      <div className="flex w-[90%] h-[100%] items-center">
+                        <div className="w-96 h-1 bg-gray-600"></div>
+                        <div className="w-[14.4rem] h-1 bg-secondary absolute drop-shadow-neonBlue"></div>
+                        <div className="h-1 w-[14.4rem] rounded-full bg-gray-200 absolute"></div>
+                      </div>
+                      <p className="ml-4 rounded-full">60%</p>
                     </div>
-                    <p className="ml-4 rounded-full">85%</p>
-                  </div>
 
-                  <div className="flex items-center mt-4">
-                    <img className="w-14 mr-2" src={JavascriptLogo} alt="Javascript"/>
-                    <div className="flex w-[90%] h-[100%] items-center">
-                      <div className="w-96 h-1 bg-gray-600"></div>
-                      <div className="w-[16.8rem] h-1 bg-primary absolute drop-shadow-neonPink"></div>
-                      <div className="h-1 w-[16.8rem] rounded-full bg-gray-200 absolute"></div>
+                    <div className="flex items-center mt-4">
+                      <img className="w-14 mr-2" src={IllustratorLogo} alt="Adobe Illustrator"/>
+                      <div className="flex w-[90%] h-[100%] items-center">
+                        <div className="w-96 h-1 bg-gray-600"></div>
+                        <div className="w-[16.8rem] h-1 bg-secondary absolute drop-shadow-neonBlue"></div>
+                        <div className="h-1 w-[16.8rem] rounded-full bg-gray-200 absolute"></div>
+                      </div>
+                      <p className="ml-4 rounded-full">70%</p>
                     </div>
-                    <p className="ml-4 rounded-full">70%</p>
-                  </div>
 
-                  <div className="flex items-center mt-4">
-                    <img className="w-14 mr-2" src={ReactLogo} alt="React framework for Javascript"/>
-                    <div className="flex w-[90%] h-[100%] items-center">
-                      <div className="w-96 h-1 bg-gray-600"></div>
-                      <div className="w-[16.8rem] h-1 bg-primary absolute drop-shadow-neonPink"></div>
-                      <div className="h-1 w-[16.8rem] rounded-full bg-gray-200 absolute"></div>
+                    <div className="flex items-center mt-4">
+                      <img className="w-14 mr-2" src={GimpLogo} alt="Gimp"/>
+                      <div className="flex w-[90%] h-[100%] items-center">
+                        <div className="w-96 h-1 bg-gray-600"></div>
+                        <div className="w-[19.2rem] h-1 bg-secondary absolute drop-shadow-neonBlue"></div>
+                        <div className="h-1 w-[19.2rem] rounded-full bg-gray-200 absolute"></div>
+                      </div>
+                      <p className="ml-4 rounded-full">80%</p>
                     </div>
-                    <p className="ml-4 rounded-full">70%</p>
-                  </div>
 
-                  <div className="flex items-center mt-4">
-                    <img className="w-14 mr-2" src={TailwindLogo} alt="Tailwind framework for CSS"/>
-                    <div className="flex w-[90%] h-[100%] items-center">
-                      <div className="w-96 h-1 bg-gray-600"></div>
-                      <div className="w-[20.4rem] h-1 bg-primary absolute drop-shadow-neonPink"></div>
-                      <div className="h-1 w-[20.4rem] rounded-full bg-gray-200 absolute"></div>
-                    </div>
-                    <p className="ml-4 rounded-full">85%</p>
-                  </div>
-
-                </section>
-
-                {/* Design/Git */}
-                <section className="w-[100%] flex flex-col justify-center">
-
-                <div className="flex items-center mt-4">
-                    <img className="w-14 mr-2" src={GitLogo} alt="Git"/>
-                    <div className="flex w-[90%] h-[100%] items-center">
-                      <div className="w-96 h-1 bg-gray-600"></div>
-                      <div className="w-[18rem] h-1 bg-secondary absolute drop-shadow-neonBlue"></div>
-                      <div className="h-1 w-[18rem] rounded-full bg-gray-200 absolute"></div>
-                    </div>
-                    <p className="ml-4 rounded-full">75%</p>
-                  </div>
-
-                  <div className="flex items-center mt-4">
-                    <img className="w-14 mr-2" src={FigmaLogo} alt="Figma"/>
-                    <div className="flex w-[90%] h-[100%] items-center">
-                      <div className="w-96 h-1 bg-gray-600"></div>
-                      <div className="w-[14.4rem] h-1 bg-secondary absolute drop-shadow-neonBlue"></div>
-                      <div className="h-1 w-[14.4rem] rounded-full bg-gray-200 absolute"></div>
-                    </div>
-                    <p className="ml-4 rounded-full">60%</p>
-                  </div>
-
-                  <div className="flex items-center mt-4">
-                    <img className="w-14 mr-2" src={IllustratorLogo} alt="Adobe Illustrator"/>
-                    <div className="flex w-[90%] h-[100%] items-center">
-                      <div className="w-96 h-1 bg-gray-600"></div>
-                      <div className="w-[16.8rem] h-1 bg-secondary absolute drop-shadow-neonBlue"></div>
-                      <div className="h-1 w-[16.8rem] rounded-full bg-gray-200 absolute"></div>
-                    </div>
-                    <p className="ml-4 rounded-full">70%</p>
-                  </div>
-
-                  <div className="flex items-center mt-4">
-                    <img className="w-14 mr-2" src={GimpLogo} alt="Gimp"/>
-                    <div className="flex w-[90%] h-[100%] items-center">
-                      <div className="w-96 h-1 bg-gray-600"></div>
-                      <div className="w-[19.2rem] h-1 bg-secondary absolute drop-shadow-neonBlue"></div>
-                      <div className="h-1 w-[19.2rem] rounded-full bg-gray-200 absolute"></div>
-                    </div>
-                    <p className="ml-4 rounded-full">80%</p>
-                  </div>
-
-                </section>
+                  </section>
+                </div>
             </div>
         </div>
     </div>
